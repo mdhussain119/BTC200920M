25358379682b38d3bf7074fd359f355c5b9bd28b
Md hussain <mdhussain@mds-macbook-pro.lan>
2021-06-06 12:47:49 -0400

    h

--- mavenHomework/src/test/java/com/bit/test/SmokeTest.java
+++ mavenHomework/src/test/java/com/bit/test/SmokeTest.java
@@ -9,27 +9,32 @@ import pages.JeansPage;
 import pages.MenPage;
 import pages.ProductPage;
 
-public class SmokeTest extends BaseTest {
-	HomePage homepage;
-	MenPage manpage;
-	HeaderPage header;
-	JeansPage jeanspage;
-	ProductPage product;
-	
+public class SmokeTest  {
 	@Test
-	public void test1() throws InterruptedException {
-		homepage = new HomePage(dr);
-		homepage.closepromotion();
-		header = new HeaderPage(dr);
-		header.headertext();
-		homepage.homepagetextdisplay();
-		manpage =homepage.clickonmen();
-		manpage.manpagetextferify();
-		jeanspage= manpage.manSubmanuText();
-		Thread.sleep(5000);
-		product =jeanspage.clickonjeansProduct();
-		Thread.sleep(5000);
-		product.clickonjeansSizesection("value", TestSample.jeanssizechart[0] );
+	public void test1() {
+		System.out.println("hi");
 	}
+//	HomePage homepage;
+//	MenPage manpage;
+//	HeaderPage header;
+//	JeansPage jeanspage;
+//	ProductPage product;
+//	
+//	@Test
+//	public void test1() throws InterruptedException {
+//		System.out.println("hi");
+//		homepage = new HomePage(dr);
+//		homepage.closepromotion();
+//		header = new HeaderPage(dr);
+//		header.headertext();
+//		homepage.homepagetextdisplay();
+//		manpage =homepage.clickonmen();
+//		manpage.manpagetextferify();
+//		jeanspage= manpage.manSubmanuText();
+//		Thread.sleep(5000);
+//		product =jeanspage.clickonjeansProduct();
+//		Thread.sleep(5000);
+//		product.clickonjeansSizesection("value", TestSample.jeanssizechart[0] );
+//	}
 
 }
